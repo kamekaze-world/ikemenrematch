847d03661463831d358de9f8df6ea830e00e2ab1
Kamekaze <ilive@kamekaze.world>
2026-04-11 22:27:02 -0400

    Update rematch_new_post3826.lua

--- rematch_new_post3826.lua
+++ rematch_new_post3826.lua
@@ -291,7 +291,7 @@ end
 					ik_rematch.done[side]=true
 				end
 			
-				if (commandGetState(side, '$U')  or commandGetState(side, '$D'))   then
+				if (getInput({side}, 'U')  or getInput({side}, 'D'))   then
 				
 					sndPlay(motif.Snd, ik_rematch.sd.rematch['p'..side].cursor.snd[1], ik_rematch.sd.rematch['p'..side].cursor.snd[2])
 					if ik_rematch.cursor[side] then
@@ -483,7 +483,7 @@ end
 					ik_rematch.done[side]=true
 				end
 
-				if (commandGetState(side, '$U')  or commandGetState(side, '$D'))   then
+				if (getInput({side}, '$U')  or getInput({side}, '$D'))   then
 				
 					sndPlay(motif.Snd, ik_rematch.sd.rematch['p'..side].cursor.snd[1], ik_rematch.sd.rematch['p'..side].cursor.snd[2])
 					if ik_rematch.cursor[side] then
